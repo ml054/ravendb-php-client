ba2d34ae8cadd61a8fbd53587e610a063ab29776
initxlab <jmfmah@gmail.com>
2021-03-18 21:47:42 +0100

    Ravendb php client - dev - committing

--- src/ravendb/Client/Http/LogFactory.php
+++ src/ravendb/Client/Http/LogFactory.php
@@ -6,6 +6,6 @@ class LogFactory
 {
     public static function getLog(object $instance)
     {
-        // TODO: Implement a logger
+        // TODO: To check with Marcin if logs should increment a file or stored elsewhere (cloud and so).
     }
 }
