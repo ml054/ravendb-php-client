b77f82c525b0e133b78f545e87a1166dbc2d9443
initxlab <jmfmah@gmail.com>
2021-03-24 10:20:45 +0100

    Ravendb php client - dev - accessing annotations. to confirm if it's the approach to implement

--- src/ravendb/Client/Serverwide/Mapper/CaseStudy/Order.php
+++ src/ravendb/Client/Serverwide/Mapper/CaseStudy/Order.php
@@ -2,14 +2,61 @@
 
 namespace RavenDB\Client\Serverwide\Mapper\CaseStudy;
 use DateTimeInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use RavenDB\Client\Serverwide\Mapper\Annotations\MyAnnotation;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ */
 class Order {
+
     public DateTimeInterface $orderDate;
     public int $id;
+    /**
+     * @ORM\OneToMany(targetEntity=OrderLine::class, mappedBy="order")
+     */
+    private ?Collection $orderLines;
+
     public string $name;
     /** TODO : VALIDATE
-     * @MyAnnotation(mapObject="RavenDB\Client\Serverwide\Mapper\CaseStudy\OrderLine")
+     * @MyAnnotation(mapObject={
+     *     "RavenDB\Client\Serverwide\Mapper\CaseStudy\OrderLine"
+     *  }
+     * )
      */
     private string $map;
+
+    public function __construct()
+    {
+        $this->orderLines = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection|OrderLine[]
+     */
+    public function getOrderLines(): Collection|OrderLine
+    {
+        return json_decode($this->orderLines->getValues());
+    }
+
+    public function addOderLine(OrderLine $orderLine):self
+    {
+        if (!$this->orderLines->contains($orderLine)) {
+            $this->orderLines[] = $orderLine;
+            $collection = new ArrayCollection($this->orderLines->toArray());
+            $this->setOrderLines($collection);
+        }
+        return $this;
+    }
+
+    /**
+     * @param ArrayCollection|Collection|null $orderLines
+     */
+    public function setOrderLines(ArrayCollection|Collection|null $orderLines): void
+    {
+        $this->orderLines = $orderLines;
+    }
+
 }
--- src/ravendb/Client/Serverwide/Mapper/CaseStudy/OrderLine.php
+++ src/ravendb/Client/Serverwide/Mapper/CaseStudy/OrderLine.php
@@ -2,6 +2,29 @@
 
 namespace RavenDB\Client\Serverwide\Mapper\CaseStudy;
 
+use Doctrine\ORM\Mapping as ORM;
+
 class OrderLine {
+    /**
+     * @ORM\ManyToOne(targetEntity=Order::class, inversedBy="order")
+     */
+    private ?Order $order;
     public string $id;
+
+    /**
+     * @return Order|null
+     */
+    public function getOrder(): ?Order
+    {
+        return $this->order;
+    }
+
+    /**
+     * @param Order|null $order
+     */
+    public function setOrder(?Order $order): void
+    {
+        $this->order = $order;
+    }
+
 }
