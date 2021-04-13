b59fade5ccb999ffc872cc8b2548d1c04e440c8a
initxlab <jmfmah@gmail.com>
2021-04-13 09:39:18 +0200

    Ravendb php client - dev - committing - before/after event. work in progress

--- src/ravendb/Client/Documents/Session/EventDispatcher/EventHandlerSubscriber.php
+++ src/ravendb/Client/Documents/Session/EventDispatcher/EventHandlerSubscriber.php
@@ -2,6 +2,9 @@
 
 namespace RavenDB\Client\Documents\Session\EventDispatcher;
 
+use RavenDB\Client\Documents\Session\EventDispatcher\EventArgs\TopologyUpdatedEventArgs;
+use RavenDB\Client\Http\ServerNode;
+use RavenDB\Client\Http\Topology;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -17,7 +20,16 @@ class EventHandlerSubscriber implements EventSubscriberInterface
             EventHandlerDispatcher::NAME => 'onEventProceedFunction'
         ];
     }
+    /**
+     * Can clearly be improved with implementing session tokens etc... this is light version for the test purpose. Subject to evoluate
+    */
     public function onEventProceedFunction(EventHandlerDispatcher $event){
         $object = $event->getObject();
+        if($object instanceof TopologyUpdatedEventArgs){
+            $topology = $object->getTopology();
+            if($topology instanceof Topology){
+                $topology->setEtag("Et58_upgraded");
+            }
+        }
     }
 }
